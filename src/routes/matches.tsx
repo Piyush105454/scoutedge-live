e98af55f8255120b23e1932692629e37758d6298
Piyush105454 <Piyushmodi812@gmail.com>
2026-08-13 01:10:00 +0530

    Chore: Separate backend code by removing frontend files from backend branch

--- src/routes/matches.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/matches")({
-  component: MatchesLayout,
-});
-
-function MatchesLayout() {
-  return <Outlet />;
-}
